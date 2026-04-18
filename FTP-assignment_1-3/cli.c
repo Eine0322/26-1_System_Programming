4afb4e248f6fa560df6dc4f2cfe5e7ee37c33051
Eine0322 <eine113589@gmail.com>
2026-04-18 19:43:56 +0900

    add: final

--- FTP-assignment_1-3/cli.c
+++ FTP-assignment_1-3/cli.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // File Name : cli.c
-// Date : 2026/04/10
+// Date : 2026/04/18
 // OS : Ubuntu 20.04.6 LTS 64bits
 // Author : Park Hyun-ji
 // Student ID : 2024402055
@@ -10,222 +10,232 @@
 //               user commands into FTP protocol commands
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <unistd.h>     // System calls: read, write
-#include <string.h>     // String handling functions
-#include <stdlib.h>     // General utilities (exit)
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define BUF_SIZE 1024   // Buffer size definition
+#define BUF_SIZE 1024
 
 ///////////////////////////////////////////////////////////////////////////////
-// convert_command
-// ============================================================================
-// Input: int argc -> number of arguments
-//        char* argv[] -> argument list from command line
-//        char* buffer -> output buffer for FTP command
-// Output: none
-// Purpose: Convert user CLI command into corresponding FTP protocol command
+// convert_command                                                           //
+// ========================================================================= //
+// Input: int    -> Number of command line arguments                         //
+//        char** -> Array of command line argument strings                   //
+//        char* -> Buffer to store the converted FTP command                //
+// Output: void                                                              //
+// Purpose: Converts user-friendly shell commands into FTP protocol commands //
 ///////////////////////////////////////////////////////////////////////////////
 void convert_command(int argc, char *argv[], char *buffer)
 {
-    if (argc < 2) { // No command provided
-        write(2, "Error: No command\n", 18);
+    ////////////////////////////// Check Argument /////////////////////////////
+    if (argc < 2) {
+        write(2, "Error: No command\n", 18); // Write error to stderr
         exit(1);
     }
+    /////////////////////////// End of Check Argument /////////////////////////
 
     ///////////////////////////////////////////////////////////////////////
-    // ls : convert to NLST
+    // ls
     ///////////////////////////////////////////////////////////////////////
     if (strcmp(argv[1], "ls") == 0) {
 
-        strcpy(buffer, "NLST"); // Base FTP command
+        strcpy(buffer, "NLST"); // Convert 'ls' to 'NLST'
 
-        for (int i = 2; i < argc; i++) { // Append options/arguments
+        //////////////////////////// Append Options ///////////////////////////
+        for (int i = 2; i < argc; i++) {
             strcat(buffer, " ");
-            strcat(buffer, argv[i]);
+            strcat(buffer, argv[i]); // Append options or path
         }
+        ///////////////////////// End of Append Options ///////////////////////
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // dir : convert to LIST
+    // dir
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "dir") == 0) {
 
-        strcpy(buffer, "LIST"); // LIST command
+        strcpy(buffer, "LIST"); // Convert 'dir' to 'LIST'
 
-        if (argc >= 3) { // Optional path argument
+        if (argc >= 3) {
             strcat(buffer, " ");
-            strcat(buffer, argv[2]);
+            strcat(buffer, argv[2]); // Append target directory if exists
         }
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // pwd : convert to PWD
+    // pwd
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "pwd") == 0) {
 
-        if (argc > 2) { // pwd should not have arguments
+        if (argc > 2) {
             write(2, "Error: pwd does not take argument\n", 35);
             exit(1);
         }
 
-        strcpy(buffer, "PWD"); // Set command
+        strcpy(buffer, "PWD"); // Convert 'pwd' to 'PWD'
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // cd : convert to CWD or CDUP
+    // cd
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "cd") == 0) {
 
-        if (argc < 3) { // Missing directory argument
+        if (argc < 3) {
             write(2, "Error: No directory name\n", 25);
             exit(1);
         }
 
-        if (argv[2][0] == '-') { // Invalid option check
+        if (argv[2][0] == '-') {
             write(2, "Error: invalid option\n", 22);
             exit(1);
         }
 
-        if (strcmp(argv[2], "..") == 0) { // Parent directory
-            strcpy(buffer, "CDUP");
+        //////////////////////////// Path Conversion //////////////////////////
+        if (strcmp(argv[2], "..") == 0) {
+            strcpy(buffer, "CDUP"); // Use CDUP for parent directory
         }
         else {
-            strcpy(buffer, "CWD "); // Change directory command
+            strcpy(buffer, "CWD "); // Use CWD for specific directory
             strcat(buffer, argv[2]);
         }
+        ///////////////////////// End of Path Conversion //////////////////////
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // mkdir : convert to MKD
+    // mkdir
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "mkdir") == 0) {
 
-        if (argc < 3) { // No directory name
+        if (argc < 3) {
             write(2, "Error: No directory name\n", 25);
             exit(1);
         }
 
-        strcpy(buffer, "MKD"); // Base command
+        strcpy(buffer, "MKD"); // Convert 'mkdir' to 'MKD'
 
+        //////////////////////////// Handle Arguments /////////////////////////
         for (int i = 2; i < argc; i++) {
 
-            if (argv[i][0] == '-') { // Option not allowed
+            if (argv[i][0] == '-') {
                 write(2, "Error: invalid option\n", 22);
                 exit(1);
             }
 
             strcat(buffer, " ");
-            strcat(buffer, argv[i]); // Append directory names
+            strcat(buffer, argv[i]); // Add directory names to buffer
         }
+        ///////////////////////// End of Handle Arguments /////////////////////
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // delete : convert to DELE
+    // delete
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "delete") == 0) {
 
-        if (argc < 3) { // No file name
+        if (argc < 3) {
             write(2, "Error: No file name\n", 20);
             exit(1);
         }
 
-        strcpy(buffer, "DELE"); // Base command
+        strcpy(buffer, "DELE"); // Convert 'delete' to 'DELE'
 
         for (int i = 2; i < argc; i++) {
 
-            if (argv[i][0] == '-') { // Invalid option
+            if (argv[i][0] == '-') {
                 write(2, "Error: invalid option\n", 22);
                 exit(1);
             }
 
             strcat(buffer, " ");
-            strcat(buffer, argv[i]); // Append file names
+            strcat(buffer, argv[i]); // Add filenames to delete
         }
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // rmdir : convert to RMD
+    // rmdir
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "rmdir") == 0) {
 
-        if (argc < 3) { // No directory name
+        if (argc < 3) {
             write(2, "Error: No directory name\n", 25);
             exit(1);
         }
 
-        strcpy(buffer, "RMD"); // Base command
+        strcpy(buffer, "RMD"); // Convert 'rmdir' to 'RMD'
 
         for (int i = 2; i < argc; i++) {
 
-            if (argv[i][0] == '-') { // Invalid option
+            if (argv[i][0] == '-') {
                 write(2, "Error: invalid option\n", 22);
                 exit(1);
             }
 
             strcat(buffer, " ");
-            strcat(buffer, argv[i]); // Append directory names
+            strcat(buffer, argv[i]); // Add directory names to remove
         }
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // rename : convert to RNFR + RNTO
+    // rename
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "rename") == 0) {
 
-        if (argc != 4) { // Must have exactly 2 arguments
+        if (argc != 4) {
             write(2, "Error: usage rename <old> <new>\n", 32);
             exit(1);
         }
 
-        strcpy(buffer, "RNFR "); // Old file name
+        //////////////////////////// Format Rename ////////////////////////////
+        strcpy(buffer, "RNFR "); // Rename From
         strcat(buffer, argv[2]);
-        strcat(buffer, " RNTO "); // New file name
+        strcat(buffer, " RNTO "); // Rename To
         strcat(buffer, argv[3]);
+        ///////////////////////// End of Format Rename ////////////////////////
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // get : convert to RETR
+    // get
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "get") == 0) {
 
-        if (argc < 3) { // No file name
+        if (argc < 3) {
             write(2, "Error: No file name\n", 20);
             exit(1);
         }
 
-        strcpy(buffer, "RETR "); // Download command
+        strcpy(buffer, "RETR "); // Retrieve file from server
         strcat(buffer, argv[2]);
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // put : convert to STOR
+    // put
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "put") == 0) {
 
-        if (argc < 3) { // No file name
+        if (argc < 3) {
             write(2, "Error: No file name\n", 20);
             exit(1);
         }
 
-        strcpy(buffer, "STOR "); // Upload command
+        strcpy(buffer, "STOR "); // Store file to server
         strcat(buffer, argv[2]);
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // quit : convert to QUIT
+    // quit
     ///////////////////////////////////////////////////////////////////////
     else if (strcmp(argv[1], "quit") == 0) {
 
-        if (argc > 2) { // quit should not have arguments
+        if (argc > 2) {
             write(2, "Error: quit does not take argument\n", 35);
             exit(1);
         }
 
-        strcpy(buffer, "QUIT"); // Exit command
+        strcpy(buffer, "QUIT"); // Terminate session
     }
 
     ///////////////////////////////////////////////////////////////////////
-    // unknown command
+    // unknown
     ///////////////////////////////////////////////////////////////////////
     else {
         write(2, "Error: Unknown command\n", 23);
@@ -234,20 +244,22 @@ void convert_command(int argc, char *argv[], char *buffer)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// main
-// ============================================================================
-// Input: int argc -> argument count
-//        char* argv[] -> argument list
-// Output: int (exit status)
-// Purpose: Convert user input command and print FTP command to stdout
+// main                                                                      //
+// ========================================================================= //
+// Input: int    -> Argument count                                           //
+//        char** -> Argument vector                                          //
+// Output: int   -> Returns 0 on success                                     //
+// Purpose: Entry point for the client to translate and output FTP command   //
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-    char buffer[BUF_SIZE] = {0}; // Initialize buffer
+    char buffer[BUF_SIZE] = {0}; // Initialize buffer with zeros
 
-    convert_command(argc, argv, buffer); // Convert command
+    /////////////////////////// Command Process ///////////////////////////////
+    convert_command(argc, argv, buffer); // Process user input
 
-    write(STDOUT_FILENO, buffer, strlen(buffer)); // Output result
+    write(STDOUT_FILENO, buffer, strlen(buffer)); // Output the protocol command
+    //////////////////////// End of Command Process ///////////////////////////
 
-    return 0; // Program end
+    return 0;
 }
